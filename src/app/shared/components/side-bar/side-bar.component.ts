09c235af4a70f9b9e16f9e22dc35b4a4850275a8
Tomás <tomasmagno2019@gmail.com>
2024-05-06 23:25:29 -0300

    Actualización

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit{
+
+ mainMenu: {
+  defaultOptions: Array<any>,
+  accessLink: Array<any> 
+ } = { defaultOptions: [], accessLink: []}
+
+ customOptions: Array<any> = []
+
+ constructor(){}
+
+ ngOnInit(): void{
+
+ }
+
+  
 
 }
+
+
